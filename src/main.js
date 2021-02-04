1b3340898e13ef2182828ebe1ae0e26a70d957a2
noodleWrecker7 <hodgkinson.adam@gmail.com>
2021-02-04 20:11:19 +0000

    x

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import socketio from 'socket.io'
+import VueSocketIO from 'vue-socket.io'
 
 Vue.config.productionTip = false
+export const SocketInstance = socketio('https://msg.noodlewrecker.net')
+Vue.use(SocketInstance, VueSocketIO)
+
 
 new Vue({
   router,
